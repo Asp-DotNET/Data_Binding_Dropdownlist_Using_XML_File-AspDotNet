c49473806b9795d8cef96454cfeac7d9d8e9a438
zulqarnain-99 <zulqi99@gmail.com>
2020-12-11 02:13:45 +0500

    bind data with dropdown list using XML File

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace Data_Binding_Dropdownlist_Using_XML_File_AspDotNet
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet DS = new DataSet();
+            string XmlFilePhysicalPath = Server.MapPath("Countries.xml");
+            DS.ReadXml(XmlFilePhysicalPath);
+            DropDownList1.DataTextField = "CountryName";
+            DropDownList1.DataValueField = "CountryId";
+            DropDownList1.DataSource = DS;
+            DropDownList1.DataBind();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
